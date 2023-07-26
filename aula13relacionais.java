4c48f1a50f9e51b4bb37a17aa7c2f78dafb6206b
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-07-26 20:45:39 -0300

    aula13relacionais.java

---
+++ aula13relacionais.java
@@ -0,0 +1,24 @@
+package cursobasicojava;
+
+public class Aula13OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		//relacionais
+				// == igual 
+				// != diferente de 
+				//> maior que
+				//< menor que
+				//>= maior ou igual que
+				//<= menor ou igual que
+				
+				int valor1=1;
+				int valor2=2;
+
+				System.out.println("valor1 == valor2: " + (valor1==valor2));
+				System.out.println("valor1 != valor2: " + (valor1!= valor2));
+				System.out.println("valor1 > valor2: " + (valor1>valor2));
+				System.out.println("valor1 => valor2: " + (valor1>=valor2));
+				System.out.println("valor1 < valor2: " + (valor1<valor2));
+				System.out.println("valor1 <= valor2: " + (valor1<=valor2));
+	}
+	}
