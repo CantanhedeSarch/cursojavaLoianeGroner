8b04f0b577769706b939266920c2f0a4c1ef3ee6
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-07-26 20:43:41 -0300

    aula13aritimeticos.java

---
+++ aula13aritimeticos.java
@@ -0,0 +1,69 @@
+package cursobasicojava;
+
+public class Aula13OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		 
+		//Aritimeticos
+			//+adição
+		//-subtração
+		//*multiplicação
+		// /divisão
+		// % resto da divisão
+		// ++ adicionar 1
+		// -- subtrair 1
+		
+		
+		
+		int resultado =  1+2;
+		System.out.println(resultado);
+		
+		resultado = resultado-1;
+		System.out.println(resultado);
+		
+		resultado = resultado*2;
+		System.out.println(resultado);
+		
+		resultado = resultado/2;
+		System.out.println(resultado);
+		
+
+		resultado = resultado+8;
+		System.out.println(resultado);
+
+		resultado = resultado%7;
+		System.out.println(resultado);
+		
+		String primeiroNome= "Essa é";
+		String segundoNome= " uma String concatenada. ";
+		String terceiroNome= primeiroNome+ segundoNome;
+		
+		System.out.println(terceiroNome);
+		
+		
+		resultado= resultado +1;
+		System.out.println(resultado);
+		
+		resultado++;// ++ significa que esta sendo adicionado 1
+		System.out.println(resultado);
+	//5
+		System.out.println(resultado++);
+		System.out.println(++resultado);
+		// a mesma coisa é aplicada para subtração
+		
+		
+		
+	
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
