bc6a13589d647b3acd56d0ff2c58c30443994db4
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-07-26 15:30:56 -0300

    aula11prt4.java
    declaração de boolean

---
+++ aula11prt4.java
@@ -0,0 +1,15 @@
+package cursobasicojava;
+
+public class Aula11VariaveisBoolean {
+
+	public static void main(String[] args) {
+		boolean verdadeiro = true;
+		boolean falso = false; 
+		 
+		System.out.println("O valor de verdadeiro é " +verdadeiro);
+		System.out.println("O valor de falso é " +falso);
+		
+
+	}
+
+}
