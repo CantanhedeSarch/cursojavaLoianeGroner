597ff5294123788f025142852d1fa82256ae412d
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-07-26 15:32:22 -0300

    curiosidadeint.java
    curiosidade de java

---
+++ curiosidadeint.java
@@ -0,0 +1,12 @@
+package cursobasicojava;
+
+public class CuriosidadeAula11 {
+
+	public static void main(String[] args) {
+		int var1= 2147483647;//valor maximo que o tipo int pode receber
+		int var2 = 100;
+ System.out.println(var1+var2);
+ //Isso acontece porque é uma roleta, logo o próximo valor da roleta após a excedência do valor máximo será o menor valor.
+	}
+
+}
