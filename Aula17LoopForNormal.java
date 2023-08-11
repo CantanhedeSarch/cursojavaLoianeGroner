419c6815a3c4e9cf4403485219acd3de14073900
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-08-11 08:22:42 -0300

    Aula17LoopForNormal.java

---
+++ Aula17LoopForNormal.java
@@ -0,0 +1,20 @@
+ackage cursobasicojava;
+
+public class Aula17LoopForNormal {
+
+	public static void main(String[] args) {
+		// Executa um bloco de codigo que esta dentro do comando for enquanto uma expressão for verdadeira
+		//exemplo do for "normal"
+		// variaveis declarada dentro do bloco de codigo for, são usadas apenas para for, em caso de uso da variavel fora do bloco for, da erro.
+		
+		for(int i=0; i<5;i++) {
+			System.out.println("i tem valor de:  "+i);
+		}
+		
+		for(int i=5; i>0;i--) {
+			System.out.println("i tem valor de:  "+i);
+		}
+		
+	}
+
+}
