3c0a30b4b95e0f5e5544f976ccf098c7c5ef4584
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-08-15 22:25:46 -0300

    aula20Arrays_Multidimensionais.java

---
+++ aula20Arrays_Multidimensionais.java
@@ -0,0 +1,45 @@
+package cursobasicojava;
+
+public class Aula20Arrays_Multidimensionais {
+
+	public static void main(String[] args) {
+		// matrizes
+		
+		//array lista ordenada de elementos
+		//matriz tabela de nxm elementos 
+		
+
+		//problema: armazenar 4 notas so ano de 30 alunos
+		
+		double [][] notasAlunos= new double [30] [4];
+		notasAlunos[0] [0] =10;
+		notasAlunos[0] [1] =8;
+		notasAlunos[0] [2] =9;
+		notasAlunos[0] [3] =8;
+		
+		notasAlunos[1] [0] =7;
+		notasAlunos[1] [1] =8;
+		notasAlunos[1] [2] =9;
+		notasAlunos[1] [3] =8.5;
+		
+		notasAlunos[2] [0] =10;
+		notasAlunos[2] [1] =9;
+		notasAlunos[2] [2] =9;
+		notasAlunos[2] [3] =8;
+		
+		notasAlunos[3] [0] =16;
+		notasAlunos[3] [1] =8;
+		notasAlunos[3] [2] =9;
+		notasAlunos[3] [3] =5;
+		
+		notasAlunos[4] [0] =9;
+		notasAlunos[4] [1] =7;
+		notasAlunos[4] [2] =9;
+		notasAlunos[4] [3] =8;
+		
+		
+		
+		
+	}
+
+}
