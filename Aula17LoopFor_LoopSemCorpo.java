140392a60a78873cc8f24cb2b13ed5879b8d2643
Sarah Andrade <128187105+CantanhedeSarch@users.noreply.github.com>
2023-08-15 22:05:31 -0300

    Aula17LoopFor_LoopSemCorpo.java

---
+++ Aula17LoopFor_LoopSemCorpo.java
@@ -0,0 +1,15 @@
+package cursobasicojava;
+
+public class Aula17LoopFor_LoopSemCorpo {
+
+	public static void main(String[] args) {
+		// serve para somar valores
+
+	 int soma = 0;
+	 
+	  for(int i=1;i<5;soma+= i++) ;
+	  System.out.println("o valor da soma é: " + soma);
+	  
+	}
+ 
+} 
